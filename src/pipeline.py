a48d7b80654b9504c3a748e05744592f0ba9bd65
Elizabeth Seaton <60676740+lizzie231@users.noreply.github.com>
2026-09-04 15:08:02 -0400

    Create cleaned sales data output

--- src/pipeline.py
+++ src/pipeline.py
@@ -2,6 +2,8 @@ import csv
 
 file_path = "data/raw_sales_data.csv"
 
+cleaned_data = []
+
 with open(file_path, "r") as file:
     reader = csv.DictReader(file)
 
@@ -9,4 +11,24 @@ with open(file_path, "r") as file:
         row["quantity"] = int(row["quantity"])
         row["unit_price"] = float(row["unit_price"])
         row["total_price"] = row["quantity"] * row["unit_price"]
-        print(row)
+        cleaned_data.append(row)
+
+output_file = "data/cleaned_sales_data.csv"
+
+fieldnames = [
+    "order_id",
+    "order_date",
+    "customer_id",
+    "customer_name",
+    "product",
+    "category",
+    "quantity",
+    "unit_price",
+    "state",
+    "total_price"
+]
+
+with open(output_file, "w", newline="") as file:
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(cleaned_data)
