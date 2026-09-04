42c39355b98baf96fa0d91b55f7ab486eca62ba7
Elizabeth Seaton <60676740+lizzie231@users.noreply.github.com>
2026-09-04 14:50:42 -0400

    Transform sales data and calculate total price

--- src/pipeline.py
+++ src/pipeline.py
@@ -6,4 +6,7 @@ with open(file_path, "r") as file:
     reader = csv.DictReader(file)
 
     for row in reader:
+        row["quantity"] = int(row["quantity"])
+        row["unit_price"] = float(row["unit_price"])
+        row["total_price"] = row["quantity"] * row["unit_price"]
         print(row)
